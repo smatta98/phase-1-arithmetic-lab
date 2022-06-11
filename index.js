d0e14c48e202ffb1934854a77ef8dd36a6f644e1
Shiv Matta <shiv.matta@gmail.com>
2022-06-11 00:42:51 -0400

    commit tree 944245a0cea9568df79797dd4c95cd64ae20b932

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2; 
+const num2 = 31; 
+const multiply = num1*num2;
 
-// Write your code here
+const random = Math.ceil(Math.random())*10;
+
+const num3 = 48;
+const num4 = 11; 
+const mod = num3 % num4;
+
+const max = Math.max(0,1,5,6,20);
+
